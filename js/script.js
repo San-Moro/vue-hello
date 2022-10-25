5d345aaf9e853e4ef109ab7e075ef71622adf9cd
San-Moro <valentinacortese.dsg@gmail.com>
2022-10-25 16:24:09 +0200

    tried to add image

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
 const {createApp} = Vue;
 // Invocare la funzione
 createApp({
     data: function(){
         return {
-            message : "Hello!"
+            message : "Hello!",
+            image : "hello-vue.jpg",
         }
     }
 }).mount("#app");
